657f3a7f169416bf6609a48f5972937f0f8ad4f1
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2025-02-16 14:23:27 +0530

    Update corsConfig.js

--- backend/corsConfig.js
+++ backend/corsConfig.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: [
-    "https://ai-content-livid.vercel.app/" // Production frontend
+    "https://ai-content-livid.vercel.app" // Production frontend
   ],
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type,Authorization",
