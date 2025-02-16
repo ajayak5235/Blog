53d76663087d330a01d8af1c14505c0a640540c9
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2025-02-16 14:23:12 +0530

    Update corsConfig.js

--- backend/corsConfig.js
+++ backend/corsConfig.js
@@ -2,7 +2,6 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: [
-    "http://localhost:5173", // Local development frontend
     "https://ai-content-livid.vercel.app/" // Production frontend
   ],
   methods: "GET,POST,PUT,DELETE,OPTIONS",
